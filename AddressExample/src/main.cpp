f54fbf88de11f3a0fe4c60fbf1f4be1ea8201b74
Connor <wxj995@gmail.com>
2024-06-28 15:28:05 -0700

    print full address

--- AddressExample/src/main.cpp
+++ AddressExample/src/main.cpp
@@ -30,11 +30,19 @@ int main(){
 
 	int a = 10;
 	int *p_int = &a;
+
 	cout<<"a = "<< a << endl;
-	cout<<"&a = " << &a << endl;
-	cout<<"*p_int = " << *p_int <<endl;
-	cout<<"p_int = " << p_int <<endl;
+	//cout<<"&a = " << &a << endl;
+	printf("&a = %p\n", &a);
+	cout<<"sizeof(a) = " << sizeof(a) << endl;
+	cout<<"typeid(a).name() = " << typeid(a).name() << endl;
 	
+	cout<< endl;
+	cout<<"*p_int = " << *p_int <<endl;
+	//cout<<"p_int = " << p_int <<endl;
+	printf("p_int = %p\n", p_int);
+	cout<<"sizeof(p_int) = " << sizeof(p_int) << endl;
+	cout<<"typeid(p_int).name() = " << typeid(p_int).name() << endl;
 
     return 0;
 }
--- README.md
+++ README.md
@@ -6,7 +6,18 @@ Go to the project dir
 > make  
   
 
+# Output Example
+```
+a = 10
+&a = 000000000069fe04
+sizeof(a) = 4
+typeid(a).name() = i
 
+*p_int = 10
+p_int = 000000000069fe04
+sizeof(p_int) = 8
+typeid(p_int).name() = Pi
+```
 
 
 # Reference
